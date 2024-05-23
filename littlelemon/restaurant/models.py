bdf28a1b593330819ec695323eb3b42f30ba0b19
samor-isa <samor.isa@biocatch.com>
2024-05-24 02:18:06 +0300

    adding fixes

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,18 +1,22 @@
-from string import digits
-from unicodedata import decimal
 from django.db import models
 
 # Create your models here.
 class Booking(models.Model):
-    id = models.IntegerField()
+    #id = models.IntegerField()
     name = models.CharField(max_length=255)
-    no_of_guests = models.IntegerField()
+    no_of_guests = models.SmallIntegerField()
     bookingDate = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return super().__str__()
 
 
 class Menu(models.Model):
-    id = models.IntegerField()
+    #id = models.SmallIntegerField()
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10,decimal_places=2)
-    inventory = models.IntegerField()
+    inventory = models.SmallIntegerField()
+
+    def __str__(self) -> str:
+        return super().__str__()
 

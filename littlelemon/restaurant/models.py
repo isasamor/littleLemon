c5e1c4b927fec0b4f2a554bdb926913c4ac785f5
samor-isa <samor.isa@biocatch.com>
2024-05-25 00:38:31 +0300

    add permissions - authenrications

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -6,8 +6,8 @@ class Booking(models.Model):
     no_of_guests = models.SmallIntegerField()
     bookingDate = models.DateTimeField()
 
-    def __str__(self) -> str:
-        return super().__str__()
+    def __str__(self):
+        return f'{self.name} : {str(self.no_of_guests)}'
 
 
 class Menu(models.Model):
@@ -15,6 +15,6 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10,decimal_places=2)
     inventory = models.SmallIntegerField()
 
-    def __str__(self) -> str:
-        return super().__str__()
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
 
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics, viewsets, permissions
 from .models import Booking, Menu
 from .serializer import bookingSerializer, menuSerializer
+from rest_framework.decorators import permission_classes
 
 
+@permission_classes([permissions.IsAuthenticated])
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = menuSerializer
+    #permission_classes = [permissions.IsAuthenticated]
 
 class SingelMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
